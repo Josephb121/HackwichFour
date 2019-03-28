55c79d4b11c754549235df795c4c6f2bc0fd6a17
CM Student <cmstudent@uhwo-acm-13.local>
2019-03-28 10:51:55 -1000

    Hackwich #6 Updated

--- HackwichFour/ViewController.swift
+++ HackwichFour/ViewController.swift
@@ -23,14 +23,27 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         cell.textLabel?.text = text
         return cell
         
+        
     }
     
-
+var restaurantImageData = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        let path = Bundle.main.path(forResource: "Property List", ofType: "plist")
+        let dict = NSDictionary(contentsOfFile: path!)
+        restaurantImageData = dict!.object(forKey:"restaurantImages") as! [String]
     }
-
-
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath as IndexPath, animated: true)
+    }
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "mySegue"
+        {
+            let s1 = segue.destination as! detailViewController
+            let imageIndex = tableView.indexPathForSelectedRow?.row
+            s1.imagePass = restaurantImageData[imageIndex!]
+    }
 }
 
+}
